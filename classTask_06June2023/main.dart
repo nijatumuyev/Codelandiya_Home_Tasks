867390922372dfb3df29966ae7d673e328882fe1
NijatUmuyev <nijatumusoy@gmail.com>
2023-06-07 14:25:50 +0400

    ClassTask(06 June 2023)

--- classTask_06June2023/main.dart
+++ classTask_06June2023/main.dart
@@ -1,7 +1,7 @@
 import 'rectangle.dart';
 
 void main() {
-  Rectangle start = Rectangle(x1: 15,x2: 20.5);
+  Rectangle start = Rectangle(x1: 15.42,x2: 20.83);
   print(start.area());
   print(start.perimeter());
 }
